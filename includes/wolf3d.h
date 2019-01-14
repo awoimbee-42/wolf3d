cd93c6c2add7b8782f5563926d57f8925b10b176
William TA <wta@e3r5p15.42.fr>
2019-01-14 17:38:02 +0100

    [DEV_READ_FILE] WIP 1.3 : Structures added in .h

--- includes/wolf3d.h
+++ includes/wolf3d.h
@@ -6,7 +6,7 @@
 /*   By: wta <marvin@42.fr>                         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/14 09:48:50 by wta               #+#    #+#             */
-/*   Updated: 2019/01/14 15:41:03 by wta              ###   ########.fr       */
+/*   Updated: 2019/01/14 16:52:55 by wta              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,12 +19,25 @@
 # define GNL_ERR	5
 # define TOKENS		"01"
 
+typedef struct	s_vec2
+{
+	double	x;
+	double	y;
+}				t_vec2;
+
 typedef struct	s_map
 {
 	char	**map;
 	int		width;
 	int		height;
 }				t_map;
+
+typedef struct	s_player
+{
+	t_vec2	pos;
+	int		fov;
+	int		angle;
+}				t_player;
 
 int		read_file(char *file, t_map *map_info);
 
