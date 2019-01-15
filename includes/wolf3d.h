86464fa506a0eb5dff12ff8a8a36344a8ec4c1f3
Arthur WOIMBEE <awoimbee@e3r1p8.42.fr>
2019-01-15 15:38:46 +0100

    added read_texture function/file

--- includes/wolf3d.h
+++ includes/wolf3d.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   wolf3d.h                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: wta <marvin@42.fr>                         +#+  +:+       +#+        */
+/*   By: awoimbee <awoimbee@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/14 09:48:50 by wta               #+#    #+#             */
-/*   Updated: 2019/01/14 16:52:55 by wta              ###   ########.fr       */
+/*   Updated: 2019/01/15 15:31:10 by awoimbee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,10 @@
 # define BAD_FMT	3
 # define MALLOC_ERR	4
 # define GNL_ERR	5
+# define READ_ERR	6
 # define TOKENS		"01"
+# define TEX_WIDTH	640
+# define TEX_HEIGHT	640
 
 typedef struct	s_vec2
 {
@@ -30,6 +33,7 @@ typedef struct	s_map
 	char	**map;
 	int		width;
 	int		height;
+	int		**textures;
 }				t_map;
 
 typedef struct	s_player
@@ -40,6 +44,7 @@ typedef struct	s_player
 }				t_player;
 
 int		read_file(char *file, t_map *map_info);
+int		**read_textures(mlx_ptr_t *mlx_ptr);
 
 int		check_bounds(t_map *map_info);
 
---
+++ srcs/read_textures.c
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   read_textures.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: awoimbee <awoimbee@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/01/15 14:33:35 by awoimbee          #+#    #+#             */
+/*   Updated: 2019/01/15 15:38:08 by awoimbee         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "wolf3d.h"
+#include "../libft/includes/libft.h"
+#include "..libft/includes/ft_printf.h"
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/uio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+int			error_exit(int msg)
+{
+	if (msg == MALLOC_ERR)
+		write(1, "Error, could not allocate memory!\n", 34);
+	else if (msg == READ_ERR)
+		write(1, "An error occured while reading files!\n", 38);
+	exit(EXIT_FAILURE);
+}
+
+int		**read_textures(mlx_ptr_t *mlx_ptr)
+{
+	int			**textures;
+	char		i;
+	char		filename[256];
+	int			fd;
+
+
+	if (!(textures = (int**)malloc(sizeof(int*) * 4))
+		|| !(filename = ft_strnew(12)))
+		error_exit(MALLOC_ERR);
+	ft_strcpy(filename, "./textures/");
+	ft_strcpy(filename + 12, ".XPM");
+	i = -1;
+	while (++i < 4)
+	{
+		filename[11] = i + '0';
+		texture[i] =
+		(int*)mlx_xpm_file_to_image(mlx_ptr, filename, TEX_WIDTH, TEX_HEIGHT);
+	}
+}
