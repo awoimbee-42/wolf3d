48cf79dabdaae9b7fd840d1850448ad82b9f313b
William TA <wta@e1r9p15.42.fr>
2019-01-17 13:15:00 +0100

    fix t_img

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: awoimbee <awoimbee@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/14 09:49:22 by wta               #+#    #+#             */
-/*   Updated: 2019/01/17 12:20:33 by wta              ###   ########.fr       */
+/*   Updated: 2019/01/17 13:13:43 by wta              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -85,7 +85,7 @@ int	key_move(int keycode, void *param)
 int		main(int ac, char **av)
 {
 	t_info	info;
-//	t_img	*img;
+	t_img	*img;
 	int		err_id;
 
 	err_id = 1;
@@ -98,14 +98,13 @@ int		main(int ac, char **av)
 		{
 			if (!(init_mlx(&info.mlx)))
 				return (0);
-			raycasting(&info);
+//			raycasting(&info);
+//			mlx_put_image_to_window(info.mlx.mlx_ptr, info.mlx.win_ptr,
+//									info.mlx.img_ptr, 0, 0);
+//	how to use read textures
+			img = read_textures(info.mlx.mlx_ptr);
 			mlx_put_image_to_window(info.mlx.mlx_ptr, info.mlx.win_ptr,
-									info.mlx.img_ptr, 0, 0);
-/*	how to use read textures
-**			img = read_textures(info.mlx.mlx_ptr);
-**			mlx_put_image_to_window(info.mlx.mlx_ptr, info.mlx.win_ptr,
-**									img[1].img_ptr, 0, 0);
-*/
+									img[1].img_ptr, 0, 0);
 			mlx_hook(info.mlx.win_ptr, 2, 0, key_move, &info);
 			mlx_loop(info.mlx.mlx_ptr);
 		}
